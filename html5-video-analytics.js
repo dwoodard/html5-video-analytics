b9ae112f28f1fa9dee6691faf6f2487ad324ab81
Dustin Woodard <dustinwoodard@weber.edu>
2015-07-21 10:24:45 -0600

    added bower.json and test

--- html5-video-analytics.js
+++ html5-video-analytics.js
@@ -1,10 +1,9 @@
 (function () {
-	
 	function analitics(string, context){
 		console.log(string, context)
 
 		if (window == this) {
-			return new hva(str, context);
+			return new analitics(str, context);
 		};
 	}
 
---
+++ test/tests.js
@@ -0,0 +1,16 @@
+/*
+it should 
+	be
+*/
+
+QUnit.test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
+
+test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
+
+test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
