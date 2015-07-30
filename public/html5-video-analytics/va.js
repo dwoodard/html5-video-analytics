a24546eac168c306f21d9910cd62e9a7d819f64c
Dustin Woodard <dustinwoodard@weber.edu>
2015-07-30 16:54:20 -0600

    Ahhhhh!!!!

--- public/html5-video-analytics/va.js
+++ public/html5-video-analytics/va.js
@@ -1,14 +1,4 @@
 /*
- __     ___     _                 _                _       _   _
- \ \   / (_) __| | ___  ___      / \   _ __   __ _| |_   _| |_(_) ___ ___
- \ \ / /| |/ _` |/ _ \/ _ \    / _ \ | '_ \ / _` | | | | | __| |/ __/ __|
- \ V / | | (_| |  __/ (_) |  / ___ \| | | | (_| | | |_| | |_| | (__\__ \
- \_/  |_|\__,_|\___|\___/  /_/   \_\_| |_|\__,_|_|\__, |\__|_|\___|___/
- |___/
- */
-
-
-/*
  USAGE:
 
  //javascript
@@ -19,27 +9,36 @@
 ;
 (function (window) {
     'use strict'
-
     function defineVA() {
-        va.fn.init = function(selector){ return selector };
-        va = function (selector) { return new va.init(selector) };
+        var va;
+        va = function (selectors) {
+            va.videos = document.body.getElementsByTagName(selectors);
+        };
+
         va.version = "0.0.0";
-        va.init.init = function(obj){};
-        va.videos = [];
-        va.events = {}; 
+
+        va.events = {};
 
 
         // set the default log handlers
-        va.log = function () { console.log.apply(console, arguments); };
-        va.warn = function () { console.warn.apply(console, arguments); };
-        va.error = function () { console.error.apply(console, arguments); };
+        //va.log = function () { console.log.apply(console, arguments); }
+        //va.warn = function () { console.warn.apply(console, arguments); }
+        //va.error = function () { console.error.apply(console, arguments); }
 
-        va.prototype.videos = {};
 
-        return va;
+        va.prototype.videos = {}
+
+        return va
     }
 
     if (typeof(va) === 'undefined') {
         window.va = defineVA()
     }
 })(window);
+
+
+
+
+
+
+
--- public/test/tests.js
+++ public/test/tests.js
@@ -1,13 +1,15 @@
 QUnit.module("Init");
 test("va()", function( assert ) {
     var videoEl = document.createElement("video");
-
+    console.log(document.body.getElementsByTagName("video"))
 	// Basic constructor's behavior
 	assert.ok(va, "va");
+	assert.ok(va, "va");
 	assert.ok(va(), "va()");
-	assert.ok(va(['test']), "va(['test'])");
+	assert.ok(va('[id]'), "va(['test'])");
+	assert.ok(va('[class]'), "va(['test'])");
 
-	// assert.equal(va(), {}, 'message');
+	// assert.equal(va(), , 'message');
 
 });
 
