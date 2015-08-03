f4f18d54da97ed75bfcb413c32c106651e726f59
Dustin Woodard <dustin.woodard@gmail.com>
2015-08-02 19:12:39 -0600

    constructor

--- public/html5-video-analytics/va.js
+++ public/html5-video-analytics/va.js
@@ -7,40 +7,55 @@
 
 
 ;(function(window){
-    window.va = function(selector){return va.fn.init(selector)};
+    window.va = function(selector){return va.fn.init(arguments)};
     va.version = "1.0.0";
     va.fn = {
         init: function(selector){
-            if (selector === 'undefined') {
+            console.log('init', selector);
+
+            if (!va.fn.isEmpty(selector) ||
+                selector === 'undefined' ||
+                arguments.length === 0) {
                 return [];
             };
 
-            if (typeof arguments[0] === 'object') {
-
+            if (Array.isArray(selector)) {
                 return [];
-
-
             };
 
             if (typeof arguments[0] === 'string') {
-
-                var arg = arguments[0].replace(/\s/g,"")
-
-
+                var arg = arguments[0].replace(/\s/g,"");
+                var hasComma = !(arg.indexOf(',') === -1);
+                if (hasComma) {
+                    arg = arg.split(',')
+                    return arg
+                }else{
+                    return arg
+                }
+            };
+            if (typeof arguments[0] === 'Object') {
+                return [arguments[0]]
             };
 
+
             return arg;
+        },
+        isEmpty: function(obj){
+            var hasOwnProperty = Object.prototype.hasOwnProperty;
+            if (obj == null) return true;
+            if (obj.length > 0)    return false;
+            if (obj.length === 0)  return true;
+
+            for (var key in obj) {
+                if (hasOwnProperty.call(obj, key)) return false;
+            }
+
+            return true;
         }
+        
     };
 
-    
+    if (typeof va == undefined) {
+            return new va()
+        };
 })(window);
-
-
-
-
-
-
-
-
-
--- public/test/tests.js
+++ public/test/tests.js
@@ -1,16 +1,26 @@
+test("Basic requirements", function() {
+	expect(5);
+	ok( Array.prototype.push, "Array.push()" );
+	ok( Function.prototype.apply, "Function.apply()" );
+	ok( document.getElementById, "getElementById" );
+	ok( document.getElementsByTagName, "getElementsByTagName" );
+	ok( RegExp, "RegExp" );
+});
+
 QUnit.module("Init");
 test("va()", function( assert ) {
     var videoEl = document.createElement("video");
 	// Basic constructor's behavior
 	assert.ok(va, "va");
-	assert.ok(va.version, "va.version");
-	assert.notOk(va(), "va()");
-	assert.ok(va({}), "va({})");
-	assert.ok(va('test'), "va('test')");
-	assert.equal(va('test1, #test2, .test3'), ["test1","#test2",".test3"], "va('test1, #test2, .test3')");
-	assert.equal(va('#id'), ['#id'], "va('#id')");
-	assert.equal(va('.class'), ['.class'], "va('.class')");
-	assert.equal(va.version, "1.0.0");
+	assert.ok(va.version, "va.version: " + va.version );
+	assert.equal(va(), [], "va()");
+	// assert.equal(va([]), [], "va([])");
+	// assert.equal(va({}), [{}], "va({})");
+	// assert.ok(va('video'), "va('video')");
+	// assert.equal(va('video1, #video2, .video3'), ["video1","#video2",".video3"], "va('video1, #video2, .video3')");
+	// assert.equal(va('#id'), ['#id'], "va('#id')");
+	// assert.equal(va('.class'), ['.class'], "va('.class')");
+	// assert.equal(va.version, "1.0.0");
 
 	// assert.equal(va(), , 'message');
 
