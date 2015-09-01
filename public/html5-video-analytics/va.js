b39f68272e8ebc1bb8e417fb9ed1624268c71669
Dustin Woodard <dustinwoodard@weber.edu>
2015-09-01 17:14:13 -0600

    va.js tests, added buffalo.mp4

--- public/html5-video-analytics/va.js
+++ public/html5-video-analytics/va.js
@@ -1,126 +1,86 @@
 /*
- USAGE:
-
+ USAGE: point va() to html5 players
  //javascript
  va('#video,video,.video')
  */
-
-
-;
-(function (window) {
+; (function (window) {
     "use strict";
     window.va = function (selector) {
+        //noinspection JSPotentiallyInvalidConstructorUsage
         return new va.fn.init(arguments)
     };
     va.version = "1.0.0";
     va.players = [];
-    va.videos = {}; 
+    va.videos = [];
     va.fn = {
         init: function (selector) {
-            var type;
-
-            if (typeof selector[0] === "object") {
-                type = "object"
-
-                if (Array.isArray(selector[0])) {
-                    type = Array.isArray(selector[0]) ? "array" : "";
-                }
-
-            }
-
-            if (typeof selector[0] === 'string') type = "string";
-
-
-            switch (type) {
-                case "array":
+            switch (Object.prototype.toString.call(selector[0])) {
+                case "[object Array]":
                     for (var i = 0; i < selector[0].length; i++) {
                         if (typeof selector[0][i] !== "string") continue;
                         var item = document.querySelectorAll(selector[0][i])[0];
                         if (va.players.indexOf(item)) {
                             va.players.push(item);
-                        };
+                        }
                     }
-                    //console.log(type, selector[0], elements);
                     break;
-                case "string":
-                    //console.log(selector, document.querySelectorAll(selector[0])[0])
+                case "[object String]":
                     var el = document.querySelectorAll(selector[0]);
-                        if (!va.players.indexOf(el[0])) {
-                            va.players.push(el[0]);
-                        };
-
+                    if (!va.players.indexOf(el[0])) {
+                        va.players.push(el[0]);
+                    }
                     break;
-                    case "object":
-                        if (va.players.indexOf(selector[0])) {
-                            va.players.push(selector[0]);
-                        };
-
+                case "[object HTMLVideoElement]":
+                    if (va.players.indexOf(selector[0])) {
+                        va.players.push(selector[0]);
+                    }
                     break;
             }
 
-            // console.log(type, va.players);
-
             //addEventListener
-            va.fn.addEventListeners(va.players)
+            va.fn.addEventListeners(va.players);
 
             return va.players;
         },
-        addEventListeners: function(elements){
+        addEventListeners: function (elements) {
             for (var i = 0; i < va.players.length; i++) {
                 var el = elements[i];
-                if (!el) {continue}
+                if (!el) {
+                    continue
+                }
 
-                el.addEventListener('emptied', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('loadedmetadata', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('loadeddata', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('canplay', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('canplaythrough', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('playing', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('ended', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('waiting', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('ended', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('durationchange', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('timeupdate', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('play', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('pause', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('ratechange', function(e){
-                    console.log(e.type, e);
-                })
-                el.addEventListener('volumechange', function(e){
-                    console.log(e.type, e);
-                })
+                var events = [
+                    "emptied",
+                    "loadstart",
+                    "loadedmetadata",
+                    "loadeddata",
+                    "canplay",
+                    "canplaythrough",
+                    "playing",
+                    "ended",
+                    "waiting",
+                    "ended",
+                    "durationchange",
+                    "timeupdate",
+                    "play",
+                    "pause",
+                    "ratechange",
+                    "volumechange"
+                ];
+
+                events.forEach(function (event) {
+                    el.addEventListener(event, function (e) {
+                        console.log(e.type, e);
+                    });
+                });
 
 
-            };
+            }
         }
     };
     if (typeof va === 'undefined') {
+        //noinspection JSPotentiallyInvalidConstructorUsage
         return new va()
     }
+
 })(window);
--- public/test/tests.js
+++ public/test/tests.js
@@ -1,129 +1,141 @@
-test("Basic requirements", function() {
-	expect(5);
-	ok( Array.prototype.push, "Array.push()" );
-	ok( Function.prototype.apply, "Function.apply()" );
-	ok( document.getElementById, "getElementById" );
-	ok( document.getElementsByTagName, "getElementsByTagName" );
-    ok( RegExp, "RegExp" );
-
-
-});
-
 var videoEl1 = document.createElement("video");
 var videoEl2 = document.createElement("video");
 var videoEl3 = document.createElement("video");
 var videoEl4 = document.createElement("video");
 
-QUnit.module("Init", {
-    beforeEach: function() {
+var beforeAfter =  {
+    beforeEach: function () {
         // prepare something for all following tests
         document.body.appendChild(videoEl1);
         document.body.appendChild(videoEl2);
         document.body.appendChild(videoEl3);
         document.body.appendChild(videoEl4);
+
         //console.log(document.querySelectorAll('#video2'));
-        videoEl1.src = "test.mp4";
+
+        videoEl1.src = "http://dev.hva.com/test/buffalo.mp4";
         videoEl2.id = "video2";
         videoEl3.className = "video3";
 
     },
-    afterEach: function() {
+    afterEach: function () {
         // clean up after each test
         $([videoEl1, videoEl2, videoEl3, videoEl4]).remove();
     }
+};
+
+test("Basic requirements", function () {
+    ok(Array.prototype.push, "Array.push()");
+    ok(Function.prototype.apply, "Function.apply()");
+    ok(document.getElementById, "getElementById");
+    ok(document.getElementsByTagName, "getElementsByTagName");
+    ok(document.querySelectorAll, "document.querySelectorAll");
+    ok(RegExp, "RegExp");
 });
+
+
+
+QUnit.module("Init", beforeAfter);
 
 test("va()", function (assert) {
 
     // Basic constructor's behavior
     assert.ok(va, "va");
     assert.ok(va.version, "va.version: " + va.version);
-    assert.deepEqual(va(['video', '#video2', '.video3']), [videoEl1, videoEl2,videoEl3]);
-    assert.deepEqual(va('#video2'), [videoEl1, videoEl2,videoEl3], "va('#id')");
-    assert.deepEqual(va('.video3'), [videoEl1, videoEl2,videoEl3], "va('.class')");
-    assert.deepEqual(va('.video3'), [videoEl1, videoEl2,videoEl3], "va('.class') again");
-    assert.deepEqual(va(videoEl4), [videoEl1, videoEl2,videoEl3, videoEl4], "va({object})");
+    assert.deepEqual(va(['video', '#video2', '.video3']), [videoEl1, videoEl2, videoEl3]);
+    assert.ok(va.players.length == 3, "total players should be 3")
+    assert.deepEqual(va('#video2'), [videoEl1, videoEl2, videoEl3], "va('#id')");
+    assert.deepEqual(va('.video3'), [videoEl1, videoEl2, videoEl3], "va('.class')");
+    assert.deepEqual(va('.video3'), [videoEl1, videoEl2, videoEl3], "va('.class') again, Should not add a second time");
+    assert.deepEqual(va(videoEl4), [videoEl1, videoEl2, videoEl3, videoEl4], "va({object})");
+    assert.ok(va.players.length == 4, "total players should be 4")
 
 });
 
-QUnit.module("Schema");
-test('compares schema to ', function (assert) {
-    assert.deepEqual(va.videos, {});
-});
+// QUnit.module("Schema");
+// test('compare schema', function (assert) {
+//    assert.deepEqual(va.videos, [
+//        {
+//            "id":"[url]"
+//        }
+//    ]);
+// });
 
 
-QUnit.module("Video Events");
+QUnit.module("Video Events", beforeAfter);
 
 test('addEventListener', function (assert) {
-    // assert.expect( 16 );
+    assert.expect( 2 );
+
+    // var emptied = assert.async();
+    var loadedmetadata = assert.async();
+    var loadeddata = assert.async();
+
 
     // video has src
-    assert.ok(!!videoEl1.src, "video has Source");
+    // assert.ok(!!videoEl1.src, "video has Source");
     // assert.ok(!videoEl2.src, "video has No Source");
 
     videoEl1.autoplay = false;
-    videoEl1.controls=true;
+    videoEl1.controls = true;
+
+    
+         
     videoEl1.play();
-    $(videoEl1).on( "emptied", function() {
-        assert.ok( true, "emptied");
+
+
+    // $(videoEl1).on("emptied", function () {
+    //     assert.ok(true, "emptied");
+    //     emptied();
+    // });
+    $(videoEl1).on("loadedmetadata", function () {
+        assert.ok(true, "loadedmetadata");
+        loadedmetadata();
     });
-    $(videoEl1).on( "loadedmetadata", function() {
-        assert.ok( true, "loadedmetadata");
-    });
-    $(videoEl1).on( "loadeddata", function() {
-        assert.ok( true, "loadeddata");
-    });
-    $(videoEl1).on( "canplay", function() {
-        assert.ok( true, "canplay");
-    });
-    $(videoEl1).on( "canplaythrough", function() {
-        assert.ok( true, "canplaythrough");
-    });
-    $(videoEl1).on( "playing", function() {
-        assert.ok( true, "playing");
-    });
-    $(videoEl1).on( "ended", function() {
-        assert.ok( true, "ended");
-    });
-    $(videoEl1).on( "waiting", function() {
-        assert.ok( true, "waiting");
-    });
-    $(videoEl1).on( "durationchange", function() {
-        assert.ok( true, "durationchange");
-    });
-    $(videoEl1).on( "timeupdate", function() {
-        assert.ok( true, "timeupdate");
-    });
-    $(videoEl1).on( "play", function() {
-        assert.ok( true, "play");
-    });
-    $(videoEl1).on( "pause", function() {
-        assert.ok( true, "pause");
-    });
-    $(videoEl1).on( "ratechange", function() {
-        assert.ok( true, "ratechange");
-    });
-    $(videoEl1).on( "volumechange", function() {
-        assert.ok( true, "volumechange");
+    $(videoEl1).on("loadeddata", function () {
+        assert.ok(true, "loadeddata");
+        loadeddata();
     });
 
 
-    $(videoEl1).trigger("emptied");
-    $(videoEl1).trigger("loadedmetadata");
-    $(videoEl1).trigger("loadeddata");
-    $(videoEl1).trigger("canplay");
-    $(videoEl1).trigger("canplaythrough");
-    $(videoEl1).trigger("playing");
-    $(videoEl1).trigger("waiting");
-    $(videoEl1).trigger("ended");
-    $(videoEl1).trigger("durationchange");
-    $(videoEl1).trigger("timeupdate");
-    $(videoEl1).trigger("play");
-    $(videoEl1).trigger("pause");
-    $(videoEl1).trigger("ratechange");
-    $(videoEl1).trigger("volumechange");
+    
 
-    $(videoEl1).remove();
+    
+    // $(videoEl1).on("canplay", function () {
+    //     assert.ok(true, "canplay");
+    // });
+    // $(videoEl1).on("canplaythrough", function () {
+    //     assert.ok(true, "canplaythrough");
+    // });
+    // $(videoEl1).on("playing", function () {
+    //     assert.ok(true, "playing");
+    // });
+    // $(videoEl1).on("ended", function () {
+    //     assert.ok(true, "ended");
+    // });
+    // $(videoEl1).on("waiting", function () {
+    //     assert.ok(true, "waiting");
+    // });
+    // $(videoEl1).on("durationchange", function () {
+    //     assert.ok(true, "durationchange");
+    // });
+    // $(videoEl1).on("timeupdate", function () {
+    //     assert.ok(true, "timeupdate");
+    // });
+    // $(videoEl1).on("play", function () {
+    //     assert.ok(true, "play");
+    // });
+    // $(videoEl1).on("pause", function () {
+    //     assert.ok(true, "pause");
+    // });
+    // $(videoEl1).on("ratechange", function () {
+    //     assert.ok(true, "ratechange");
+    // });
+    // $(videoEl1).on("volumechange", function () {
+    //     assert.ok(true, "volumechange");
+    // });
+
+    //$(videoEl1).remove();
 });
 
 
